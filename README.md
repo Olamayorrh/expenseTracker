3e39035c8ec7659339fd76df9074ffc4ba7c2731
Olamayorrh <105288976+Olamayorrh@users.noreply.github.com>
2024-07-26 21:44:12 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# expenseTracker
+## 1. Expense Tracking App
+
+Hello, this file contains a concise description on how to use my Expense tracking App.
+This project was built, using Html, CSS and javascript(manipulating the DOM)
+
+### 2. Key points
+- How to add a budgetted amount
+- How to add an expense(s)
+- How to edit already logged expense
+- How to delete an expense
+
+### 2a. How to add a budgetted Amount
+* To add a budgetted amount, click on the box directly infront of **Your budget** text
+  and type in your budget. You can click on the *Add expense* button immediately, to add your budget or wait to fill in the rest of the provided spaces before clicking the *Add Expense* button.
+
+
+![Budget price image](./images/budget1.png.png)
+* mobile View
+
+![Budget price image](./images/budget2.png.png)
+* Desktop View
+
+### 2b. How to add an expense
+* To add an expense, fill in the spaces provided for product description, price and quantity, then click on the **add Expense** button to log an expense as shown below:
+![Add Expense](./images/firstExpense.png)
+
+* Image of expense details after adding an expense is shown below:
+![expense detail](./images/firstExpenseResult.png)
+
+* In order to add more expenses, just fill the spaces for product description, price and quantity and leave the already filled budget price, then click the **add Expense** button to add another expense .
+
+### 2c. How to edit already logged expense
+* To **edit** already logged expense, you need to first fill the product description, price and quantity, before clicking on the edit button.
+**Note**: fill these required spaces just the way you add your expenses but instead of clicking on the **Add Expense**, you click on the **edit button** as shown below:
+
+* first fill the spaces as shown below:
+![edit image1](./images/edit1.png)
+
+* Then click on the edit button as shown below:
+![edit image2](./images/edit2.png)
+
+* Result of edited expense is shown below:
+![edit image3](./images/editFinal.png)
+
+### 3d. How to delete an expense
+
+* To delete an expense, all you need to do is to click on the delete button of the expense to be deleted, just as shown in the image below:
+
+* clicking on delete button
+![delete image](./images/clickDelete.png)
+
+* Remaining expense list after deleting an expense
+![delete image](./images/shoeDeleted.png)
+
+
+
---
+++ app.js
@@ -0,0 +1,127 @@
+let budget = document.getElementById("budget");
+let productDes = document.getElementById("productDes");
+let price = document.getElementById("price");
+let quantity = document.getElementById("quantity");
+let btn = document.getElementById("btn");
+
+let budgetAmt = document.getElementById("budgetAmt");
+let totalExp = document.getElementById("totalExp");
+let totalBal = document.getElementById("totalBal");
+let list = document.querySelector(".list");
+
+let Db = [];
+
+
+
+btn.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    let budgetAmount = budget.value;
+
+    budgetAmt.innerText = budgetAmount;
+
+    if((productDes.value.length && price.value.length) > 0){
+        Db.push({
+                
+                desc: productDes.value,
+                
+                amount: parseInt(price.value) * parseInt(quantity.value)
+        });
+
+        
+
+        productDes.value = '';
+        price.value = '';
+        quantity.value = '';
+        // budget.value = '';
+
+        console.log(Db);
+        displayFunction(Db);
+
+        
+        let totalExpenses = Db.reduce((currentTotal,item)=>{
+           return (item.amount + currentTotal);
+            
+        }, 0);
+
+        totalExp.innerText = totalExpenses;
+
+        let AvailableBal = budgetAmount - totalExpenses;
+
+        totalBal.innerText = AvailableBal;
+
+
+    }
+});
+
+const displayFunction = (item)=>{
+    let Html = ``;
+
+    for(let i = 0; i < item.length; i++){
+
+        Html += 
+        
+        ` <tr>
+                
+                <td> ${item[i].desc}</td>
+                <td> ${item[i].amount} </td>
+                <td id="action"><span class="action ac1"><i class="fa-solid fa-pen-to-square" onClick="editExpense(${i})"></i></span> <span class="action ac2"><i class="fa-solid fa-trash" onClick="deleteExpense(${i})"></i></span> </td>
+          </li>
+        `
+    }
+ 
+    list.innerHTML = Html;
+}
+
+
+const editExpense = ((id)=>{
+    if((productDes.value.length && price.value.length) > 0){
+        Db.splice(id,1,{desc:productDes.value,amount:parseInt(price.value) * parseInt(quantity.value),id:id + 1});
+        
+        
+        let budgetAmount = budget.value;
+
+        productDes.value= "";
+        price.value="";
+        quantity.value="";
+
+        let totalExpenses = Db.reduce((currentTotal,item)=>{
+            return (item.amount + currentTotal);
+             
+         }, 0);
+ 
+         totalExp.innerText = totalExpenses;
+ 
+         let AvailableBal = budgetAmount - totalExpenses;
+ 
+         totalBal.innerText = AvailableBal;
+
+         displayFunction(Db);
+        console.log(Db);
+    };
+
+});
+
+const deleteExpense = ((id)=>{
+    Db.splice(id,1);
+    
+
+    let budgetAmount = budget.value;
+
+    let totalExpenses = Db.reduce((currentTotal,item)=>{
+        return (item.amount + currentTotal);
+         
+     }, 0);
+
+     totalExp.innerText = totalExpenses;
+
+     let AvailableBal = budgetAmount - totalExpenses;
+
+     totalBal.innerText = AvailableBal;
+
+     displayFunction(Db);
+    console.log(Db);
+})
+
+
+
